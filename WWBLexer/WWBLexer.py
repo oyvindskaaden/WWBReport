f1c078cbadc6a57652001086bc4926ec1aca3cfc
Øyvind Skaaden <oyvindskaaden@gmail.com>
2022-10-09 08:17:49 +0200

    Added type hints and group in list

--- WWBLexer/WWBLexer.py
+++ WWBLexer/WWBLexer.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 from pygments.lexer import RegexLexer
 from pygments.token import Token, Keyword, Generic
+import re
 
 
 class WWBLexer(RegexLexer):
@@ -14,7 +15,7 @@ class WWBLexer(RegexLexer):
     current_csv: list   = []
     current_zone: dict  = {}
 
-    def register_showname_cb(self, match):
+    def register_showname_cb(self: RegexLexer, match: re.Match):
         show_name = match.group(1)
         
         self.wwb_tree["show_name"] = show_name
@@ -24,13 +25,13 @@ class WWBLexer(RegexLexer):
         yield match.start(), Token.WWB.ShowName, show_name
 
 
-    def register_type_cb(self, match):
+    def register_type_cb(self: RegexLexer, match: re.Match):
         report_type = match.group(1)
         self.wwb_tree["type"] = report_type
         yield match.start(), Token.WWB.Type, report_type
 
 
-    def register_rf_zone_cb(self, match):
+    def register_rf_zone_cb(self: RegexLexer, match: re.Match):
         if "zones" not in self.wwb_tree:
                 self.wwb_tree["zones"] = {}
 
@@ -41,7 +42,7 @@ class WWBLexer(RegexLexer):
         yield match.start(), Token.WWB.RFZone, zone
 
 
-    def register_active_cb(self, match):
+    def register_active_cb(self: RegexLexer, match: re.Match):
         no_channels = match.group(1)
         self.current_zone["no_active"] = no_channels
         self.current_zone["active"] = []
@@ -51,7 +52,7 @@ class WWBLexer(RegexLexer):
         yield match.start(), Token.WWB.ActiveChannels, no_channels
     
 
-    def register_backup_cb(self, match):
+    def register_backup_cb(self: RegexLexer, match: re.Match):
         no_backup = match.group(1)
         self.current_zone["no_backup"] = no_backup
         self.current_zone["backup"] = []
@@ -60,18 +61,22 @@ class WWBLexer(RegexLexer):
 
         yield match.start(), Token.WWB.ActiveChannel, no_backup
 
+    def register_group_cb(self: RegexLexer, match: re.Match):
+        inclusion_group = match.group(1)
+        no_ch_in_group = match.group(2)
+        yield match.start(), Token.WWB.InclusionGroup, inclusion_group
 
-    def register_parameters_cb(self, match):
+    def register_parameters_cb(self: RegexLexer, match: re.Match):
         self.wwb_tree["parameters"] = {}
         yield match.start(), Token.WWB.Parameters, "Report Parameters"
 
 
-    def register_inclusions_cb(self, match):
+    def register_inclusions_cb(self: RegexLexer, match: re.Match):
         self.wwb_tree["parameters"]["inclusions"] = {}
         yield match.start(), Token.WWB.Inclusions, "Report Inclusions"
     
 
-    def register_user_group_cb(self, match):
+    def register_user_group_cb(self: RegexLexer, match: re.Match):
         user_group = match.group(1)
         self.wwb_tree["parameters"]["inclusions"]["user_group_name"] = user_group
         self.wwb_tree["parameters"]["inclusions"]["user_group"] = []
@@ -80,7 +85,7 @@ class WWBLexer(RegexLexer):
         yield match.start(), Token.WWB.UserGroup, user_group
 
     
-    def register_inclusion_list_cb(self, match):
+    def register_inclusion_list_cb(self: RegexLexer, match: re.Match):
         self.wwb_tree["parameters"]["inclusions"]["inclusion_list_name"] = match.group(1)
         self.wwb_tree["parameters"]["inclusions"]["inclusion_list"] = []
         self.current_csv = self.wwb_tree["parameters"]["inclusions"]["inclusion_list"]
@@ -88,13 +93,13 @@ class WWBLexer(RegexLexer):
         yield match.start(), Token.WWB.InclusionList, "Inclusion List"
 
     
-    def register_exclusions_cb(self, match):
+    def register_exclusions_cb(self: RegexLexer, match: re.Match):
         self.wwb_tree["parameters"]["exclusions"] = {}
 
         yield match.start(), Token.WWB.UserGroup, "Report Exclusions"
 
 
-    def register_active_tv_cb(self, match):
+    def register_active_tv_cb(self: RegexLexer, match: re.Match):
         tv_channels = match.group(1)
         self.wwb_tree["parameters"]["exclusions"]["no_active_tv"] = tv_channels
         self.wwb_tree["parameters"]["exclusions"]["active_tv"] = []
@@ -103,7 +108,7 @@ class WWBLexer(RegexLexer):
         yield match.start(), Token.WWB.TVChannels, tv_channels
 
     
-    def register_other_exlusions_cb(self, match):
+    def register_other_exlusions_cb(self: RegexLexer, match: re.Match):
         other_exclusions = match.group(1)
         self.wwb_tree["parameters"]["exclusions"]["no_other_exlusions"] = other_exclusions
         self.wwb_tree["parameters"]["exclusions"]["other_exlusions"] = []
@@ -112,7 +117,7 @@ class WWBLexer(RegexLexer):
         yield match.start(), Token.WWB.OtherExclusions, other_exclusions
 
 
-    def register_date_created_cb(self, match):
+    def register_date_created_cb(self: RegexLexer, match: re.Match):
         time_format = "%d %b %Y at %I:%M%p"
         created = datetime.strptime(match.group(1), time_format)
         self.wwb_tree["created"] = created.isoformat()
@@ -120,14 +125,14 @@ class WWBLexer(RegexLexer):
         yield match.start(), Token.WWB.Created, created.isoformat()
 
     
-    def register_wwb_version_cb(self, match):
+    def register_wwb_version_cb(self: RegexLexer, match: re.Match):
         version = match.group(1)
         self.wwb_tree["wwb_version"] = version
 
         yield match.start(), Token.WWB.Version, version
 
 
-    def register_csv_cb(self, match):
+    def register_csv_cb(self: RegexLexer, match: re.Match):
         self.current_csv.append(match.group(0))
         yield match.start(), Token.WWB.CSV, "CSV"
 
@@ -142,6 +147,7 @@ class WWBLexer(RegexLexer):
             (r'"Active Channels \((\d+)\)"\s+',     register_active_cb),
             (r'Backup Frequencies \((\d+)\),(?:Frequency List Source: (.+))?,\s+', 
                                                     register_backup_cb),
+            (r'\n(.+) \((\d+)\),{8}',               register_group_cb),
             
             (r'""\n"Frequency Coordination Parameters"\s', 
                                                     register_parameters_cb),
