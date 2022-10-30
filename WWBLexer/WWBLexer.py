79e52683837f2f80cdf608b357862c5f9ffa06be
Øyvind Skaaden <oyvindskaaden@gmail.com>
2022-10-30 13:34:16 +0100

    Added some post processing and output to markdown

--- WWBLexer/WWBLexer.py
+++ WWBLexer/WWBLexer.py
@@ -23,8 +23,8 @@ class WWBLexer(RegexLexer):
         show_name = match.group(1)
         
         self.wwb_tree["show_name"] = show_name
-        self.wwb_tree["contact_info"] = []
-        self.current_csv = self.wwb_tree["contact_info"]
+        self.wwb_tree["contact_info_show"] = []
+        self.current_csv = self.wwb_tree["contact_info_show"]
         
         yield match.start(), Token.WWB.ShowName, show_name
 
@@ -200,7 +200,12 @@ class WWBLexer(RegexLexer):
 
     def __post_process_wwb_tree(self):
 
-        self.wwb_tree["contact_info"] = pd.DataFrame(self.wwb_tree["contact_info"])
+        self.wwb_tree["contact_info_show"] = pd.DataFrame(
+            self.wwb_tree["contact_info_show"][1:],
+            columns=self.wwb_tree["contact_info_show"][0]
+        )
+        self.wwb_tree["contact_info_customer"] = self.wwb_tree["contact_info_show"].iloc[:,[3,4]].copy().to_dict()
+        self.wwb_tree["contact_info_show"] = self.wwb_tree["contact_info_show"].iloc[:,[0,1]].copy().to_dict()
 
         for zone in self.wwb_tree["zones"]:
             for type in self.wwb_tree["zones"][zone]:
@@ -208,24 +213,46 @@ class WWBLexer(RegexLexer):
                     if group == "header" or group.startswith("no_"):
                         continue
 
-                    self.wwb_tree["zones"][zone][type][group] = pd.DataFrame(
+                    # Move the Frequency header from GroupChannel to actual frequency.
+                    header = self.wwb_tree["zones"][zone][type]["header"]
+
+                    # Group frequency is in column 4 (5) and the actual frequencies is in 5 (6)
+                    header[4] = ""
+                    header[5] = "Frequency"
+
+                    # Create dataframe with the new custom header.
+                    # Using the same header over for multiple rf zones
+                    df = pd.DataFrame(
                         self.wwb_tree["zones"][zone][type][group],
-                        columns=self.wwb_tree["zones"][zone][type]["header"]
+                        columns=header
                     )
+                    
+                    # Drop the empty columns
+                    df.drop("", axis="columns", inplace=True)
+                    
+                    self.wwb_tree["zones"][zone][type][group] = df.to_dict()
+                
+                self.wwb_tree["zones"][zone][type].pop("header")
+
 
         for params in self.wwb_tree["parameters"]:
             for param in self.wwb_tree["parameters"][params]:
                 if param.startswith('no_') or param.endswith('_name'):
                     continue
 
-                self.wwb_tree["parameters"][params][param] = pd.DataFrame(
+                df = pd.DataFrame(
                     self.wwb_tree["parameters"][params][param][1:],
                     columns=self.wwb_tree["parameters"][params][param][0]
                 )
+                
+                # Drop the empty columns
+                df.drop("", axis="columns", inplace=True)
+
+                self.wwb_tree["parameters"][params][param] = df.to_dict()
         return
 
 
-    def get_wwb_tree(self, wwb_string: str, post_process: bool = True) -> dict:
+    def get_wwb_tree(self, wwb_string: str, post_process: bool = False) -> dict:
         # Pre process the string to fix issues with wwb format
         wwb_string = self.__pre_process_wwb_string(wwb_string)
     
@@ -242,15 +269,68 @@ class WWBLexer(RegexLexer):
 
     def __str__(self) -> str:
         
+        markdown_output = ""
 
-
-        return f"""
-# {self.wwb_tree["show_name"]}
         
-{self.wwb_tree["contact_info"].to_markdown()}
+
+        markdown_output += f"""# {self.wwb_tree["show_name"]}
+---
+
+> Created on: {datetime.fromisoformat(self.wwb_tree["created"]).strftime("%d %b %Y at %H:%M:%S")}
+
+> Created on version: {self.wwb_tree["wwb_version"]}
+
+## Venue Information
+
+{pd.DataFrame(self.wwb_tree["contact_info_show"]).to_markdown(index=False)}
+
+## Customer Information
+
+{pd.DataFrame(self.wwb_tree["contact_info_customer"]).to_markdown(index=False)}
 
 ## {self.wwb_tree["type"]}
-
-
-        #
         """
+# RF Zones
+        for zone in self.wwb_tree["zones"]:
+            markdown_output += f"""
+### RF Zone: {zone}
+            """
+            for type in self.wwb_tree["zones"][zone]:
+                markdown_output += f"""
+#### {type.capitalize()} channels ({str(self.wwb_tree["zones"][zone][type]["no_" + type])})
+                """
+
+                for group in self.wwb_tree["zones"][zone][type]:
+                    if group in "header" or group.startswith("no_"):
+                        continue
+                    markdown_output += f"""
+##### {group}
+
+{pd.DataFrame(self.wwb_tree["zones"][zone][type][group]).to_markdown(index=False)}
+"""
+
+# Frequency Coordination Parameters
+        markdown_output += """
+## Frequency Coordination Parameters
+"""
+        for param in self.wwb_tree["parameters"]:
+            markdown_output += f"""
+### {param.capitalize()} 
+"""
+
+            for list in self.wwb_tree["parameters"][param]:
+                if list.endswith("_name") or list.startswith("no_"):
+                    continue
+
+                markdown_output += f"""
+#### {list.replace("_", " ").capitalize()}"""
+
+                if (param == "inclusions"):
+                    markdown_output += " - " + self.wwb_tree["parameters"][param][list + "_name"]
+                elif (param == "exclusions"):
+                    markdown_output += f""" ({self.wwb_tree["parameters"][param]["no_" + list]})"""
+                markdown_output += f"""\n
+{pd.DataFrame(self.wwb_tree["parameters"][param][list]).to_markdown(index=False)}
+"""
+
+        return markdown_output
