fa9ba8b71bf337569c29e67917e26efc1445574d
Øyvind Skaaden <oyvindskaaden@gmail.com>
2022-11-12 13:41:28 +0100

    Added type hints and combined channel and backup

--- WWB/channel.py
+++ WWB/channel.py
@@ -1,6 +1,6 @@
 import json
 
-class Channel(dict):
+class Active(dict):
 
     ##################
     ## CONSTRUCTORS ##
@@ -42,13 +42,13 @@ class Channel(dict):
 
         # The csv string is as follows
         # Model, Band, Channel Name, Device ID, Channel/Group, Frequency, [Tags]*,,
-        model           = fields.pop(0)     # Model field
-        band            = fields.pop(0)     # Band field
-        channel_name    = fields.pop(0)     # Channel name field
-        device_id       = fields.pop(0)     # Device ID field
-        _               = fields.pop(0)     # This field is unused, the Channel/Group
-        frequency       = fields.pop(0)     # Frequency field
-        tags            = fields[:-2]       # The rest, but not the last 2, are tags
+        model           : str       = fields.pop(0)     # Model field
+        band            : str       = fields.pop(0)     # Band field
+        channel_name    : str       = fields.pop(0)     # Channel name field
+        device_id       : str       = fields.pop(0)     # Device ID field
+        _                           = fields.pop(0)     # This field is unused, the Channel/Group
+        frequency       : str       = fields.pop(0)     # Frequency field
+        tags            : list[str] = fields[:-2]       # The rest, but not the last 2, are tags
 
         return cls(model, band, channel_name, device_id, frequency, tags)
 
@@ -95,6 +95,89 @@ class Channel(dict):
         cls_str += f"Frequency:     {self.frequency}\n"
 
         cls_str += f"Tags:          {self.tags}\n"
+
+        return cls_str
+
+    ###################
+    ## OTHER METHODS ##
+    ###################
+    def to_json(self, indent: int = None):
+        return json.dumps(self.__dict__, ensure_ascii=False, indent=indent)
+
+
+class Backup(dict):
+
+    ##################
+    ## CONSTRUCTORS ##
+    ##################
+    def __init__(self,
+            type            : str,
+            band            : str,
+            frequency       : str,
+        ) -> None:
+        """
+        Form a WWB Channel Object from a list of identifiers
+        """
+
+        # Set the internal variables, as this is a dict
+        self["type"]            : str       = type
+        self["band"]            : str       = band
+        self["frequency"]       : str       = frequency
+
+        pass
+
+    @classmethod
+    def from_csvrow(
+            cls,
+            csv_line        : str
+        ):
+        """
+        Form a WWB Backup Channel Object from a csv row on the following format:
+
+        `Type,Band,,,,Frequency,,,`
+        """
+
+        fields  = csv_line.split(",")
+
+        # The csv string is as follows
+        # Model, Band, Channel Name, Device ID, Channel/Group, Frequency, [Tags]*,,
+        type            : str   = fields.pop(0)     # Type field
+        band            : str   = fields.pop(0)     # Band field
+        _                       = fields.pop(0)     # Unused, Channel name field
+        _                       = fields.pop(0)     # Unused, Device ID field
+        _                       = fields.pop(0)     # Unused, Channel/Group field
+        frequency       : str   = fields.pop(0)     # Frequency field
+        # The rest is unused
+
+        return cls(type, band, frequency)
+
+
+    ################
+    ## PROPERTIES ##
+    ################
+    @property
+    def type(self) -> str:
+        return self["model"]
+
+    @property
+    def band(self) -> str:
+        return self["band"]
+
+    @property
+    def frequency(self) -> str:
+        return self["frequency"]
+    
+
+    ######################
+    ## INTERNAL METHODS ##
+    ########################
+    def __repr__(self) -> str:
+        return super().__repr__()    
+
+    def __str__(self) -> str:
+        cls_str  = f"Type:          {self.type}\n"
+        cls_str += f"Band:          {self.band}\n"
+        cls_str += f"Frequency:     {self.frequency}\n"
 
         return cls_str
 
